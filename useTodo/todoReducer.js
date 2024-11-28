1d5ef3904fbc3a4531cc619bb884c422e0bb0e4e
Jaime Valdivia <dev@jaimevaldivia.com>
2024-11-28 08:55:41 -0600

    todoReducer dep added

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,23 @@
+const todoReducer = (state, action = {}) => {
+  switch (action.type) {
+    case 'add':
+      return [...state, action.payload];
+    case 'delete':
+      return state.filter((s) => s.id !== action.payload);
+    case 'toggle':
+      return state.map((s) => {
+        if (s.id === action.payload) {
+          return {
+            ...s,
+            done: !s.done,
+          };
+        }
+
+        return s;
+      });
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
